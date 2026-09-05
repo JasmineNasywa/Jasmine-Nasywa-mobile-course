a7033e79e8197eca4a711e06f12e77e95dc0450f
JasmineNasywa <jasminenasywa6@gmail.com>
2026-09-05 19:44:09 +0700

    w2 feat : implement vanilla version declarative UI

--- week_2_declarative_ui_and_responsive_design/lib/main.dart
+++ week_2_declarative_ui_and_responsive_design/lib/main.dart
@@ -1,68 +1,132 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-void main() => runApp(const ProfileApp());
+void main() => runApp(const DashboardApp());
 
-class ProfileApp extends StatelessWidget {
-  const ProfileApp({super.key});
+class DashboardApp extends StatelessWidget {
+  const DashboardApp ({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: ProfileCard()
-          ),
-      )
-    );
-  }
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo),
+    darkTheme: ThemeData(useMaterial3: true, brightness: Brightness.dark, colorSchemeSeed: Colors.indigo),
+    themeMode: ThemeMode.system,
+    home: const DasboardPage(),
+  );
+}
 }
 
-class ProfileCard extends StatelessWidget {
-  const ProfileCard({super.key});
+class DasboardPage extends StatelessWidget {
+  const DasboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 320,
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Colors.indigo.shade50,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
+    return Scaffold(
+      appBar: AppBar(title: const Text('Student Dasboard'),),
+      body: LayoutBuilder(
+        builder: (context, constraints){
+          final columns = constraints.maxWidth >= 700 ? 2 : 1;
+          return GridView.count(
+            padding: const EdgeInsets.all(16),
+            crossAxisCount: columns,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+            childAspectRatio: 2.6,
             children: [
-              const CircleAvatar(child: Icon(Icons.person)),
-              const SizedBox(width: 12),
-              Expanded(child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const[
-                  Text('Nama Mahasiswa',
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-                  Text('Jasmine Nasywa N')
-                ],
-              ))
+              DashboardCard(title: 'Assignments', value: '8'),
+              DashboardCard(title: 'Attendance', value: '92%'),
+              DashboardCard(title: 'Portofolio', value: 'Ready'),
+              DashboardCard(title: 'Current week', value: '02'),
             ],
-          ),
-          const SizedBox(height: 12,),
-          const Row(children: [
-            Expanded(child: Text('NIM')),
-            Text('244107020119')
-          ],),
-          const Row(children: [
-            Expanded(child: Text('Kelas')),
-            Text('TI-3G'),
-          ],),
-          const Row(children: [
-            Expanded(child: Text('Email')),
-            Text('jasminenasywa6@gmail.com'),
-          ],),
-        ],
-      ),
+          
+          );
+        }
+        ),
     );
-
   }
 }
+
+class DashboardCard extends StatelessWidget {
+  const DashboardCard({required this.title, required this.value, super.key});
+  final String title;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(padding: const EdgeInsets.all(20),
+      child: Row(children: [
+        Expanded(child: Text(title)),
+        Text(value, style: Theme.of(context).textTheme.headlineSmall),
+      ],),
+      ),
+    );
+  }
+}
+
+// class ProfileApp extends StatelessWidget {
+//   const ProfileApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: ProfileCard()
+//           ),
+//       )
+//     );
+//   }
+// }
+
+// class ProfileCard extends StatelessWidget {
+//   const ProfileCard({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 320,
+//       padding: const EdgeInsets.all(16),
+//       decoration: BoxDecoration(
+//         color: Colors.indigo.shade50,
+//         borderRadius: BorderRadius.circular(16),
+//       ),
+//       child: Column(
+//         mainAxisSize: MainAxisSize.min,
+//         children: [
+//           Row(
+//             children: [
+//               const CircleAvatar(child: Icon(Icons.person)),
+//               const SizedBox(width: 12),
+//               Expanded(child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: const[
+//                   Text('Nama Mahasiswa',
+//                   style: TextStyle(fontWeight: FontWeight.bold)),
+//                   Text('Jasmine Nasywa N')
+//                 ],
+//               ))
+//             ],
+//           ),
+//           const SizedBox(height: 12,),
+//           const Row(children: [
+//             Expanded(child: Text('NIM')),
+//             Text('244107020119')
+//           ],),
+//           const Row(children: [
+//             Expanded(child: Text('Kelas')),
+//             Text('TI-3G'),
+//           ],),
+//           const Row(children: [
+//             Expanded(child: Text('Email')),
+//             Text('jasminenasywa6@gmail.com'),
+//           ],),
+//         ],
+//       ),
+//     );
+
+//   }
+// }
